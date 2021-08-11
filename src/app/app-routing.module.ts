cd9b07e5697dc05a7cd947683b2833df5818344f
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-11 09:51:40 +0200

    Routing für die drei Hauptseiten hinzugefügt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SigninComponent } from './pages/signin/signin.component';
+
+const routes: Routes = [
+  { path: "", redirectTo: "/signin", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "signin", component: SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
