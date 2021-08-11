5b0cfb97420d4077643d18df07e16f78cac37a8c
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-11 10:57:10 +0200

    Simples Styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { GoalsDashboardComponent } from './dashboard-pages/goals-dashboard/goals-dashboard.component';
+import { MainDashboardComponent } from './dashboard-pages/main-dashboard/main-dashboard.component';
+import { SettingsComponent } from './dashboard-pages/settings/settings.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/signin", pathMatch: "full" },
   { path: "dashboard", component: DashboardComponent },
   { path: "signup", component: SignupComponent },
-  { path: "signin", component: SigninComponent}
+  { path: "signin", component: SigninComponent},
+  { path: "dashboard/goals-dashboard", component: GoalsDashboardComponent },
+  { path: "dashboard/main-dashboard", component: MainDashboardComponent },
+  { path: "dashboard/settings", component: SettingsComponent }
 ];
 
 @NgModule({
