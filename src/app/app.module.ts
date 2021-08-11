fa59eb681d3556b30042ba3baf766c5f7c9be8a1
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-11 09:44:46 +0200

    Hauptseiten generiert
    dashboard, signup und signin pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
