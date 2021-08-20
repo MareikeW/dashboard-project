12b46906372393ac73633f6fe5ea8bebb232741c
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-20 15:37:55 +0200

    diagramm init

---
+++ src/app/dashboard-pages/main-dashboard/uebersicht-diagramm/uebersicht-diagramm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uebersicht-diagramm',
+  templateUrl: './uebersicht-diagramm.component.html',
+  styleUrls: ['./uebersicht-diagramm.component.scss']
+})
+export class UebersichtDiagrammComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
